8006d3667b41b6a9db4967db551491b6bb6e503a
arimura <kotaro_arimura@voyagegroup.com>
2021-09-12 16:46:37 +0900

    Dump cookies

--- WKWebViewCookieIntro/WKWebViewCookieIntro/ViewController.swift
+++ WKWebViewCookieIntro/WKWebViewCookieIntro/ViewController.swift
@@ -15,11 +15,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        webview.navigationDelegate = self
+        
         let url = URL(string:"http://192.168.86.238:8000/hoge")!
         let req = URLRequest(url: url)
         webview.load(req)
     }
-
-
 }
 
+extension ViewController : WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!){
+        let cookieStore = webview.configuration.websiteDataStore.httpCookieStore
+        cookieStore.getAllCookies { (cookies) in
+            for cookie in cookies {
+                print(cookie)
+            }
+        }
+    }
+}
