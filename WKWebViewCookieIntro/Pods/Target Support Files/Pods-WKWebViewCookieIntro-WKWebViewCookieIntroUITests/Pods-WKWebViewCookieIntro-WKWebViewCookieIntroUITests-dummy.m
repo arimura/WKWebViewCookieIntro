72cb46afedd8993b4e347e80c5fb051c19a5a300
arimura <kotaro_arimura@voyagegroup.com>
2021-09-13 11:50:49 +0900

    Add snapkit

---
+++ WKWebViewCookieIntro/Pods/Target Support Files/Pods-WKWebViewCookieIntro-WKWebViewCookieIntroUITests/Pods-WKWebViewCookieIntro-WKWebViewCookieIntroUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WKWebViewCookieIntro_WKWebViewCookieIntroUITests : NSObject
+@end
+@implementation PodsDummy_Pods_WKWebViewCookieIntro_WKWebViewCookieIntroUITests
+@end
